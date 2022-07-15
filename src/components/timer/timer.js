db53077f05296240d831292229eb1dbc5d0bcc81
Steven Olds <nathanolds02@gmail.com>
2022-07-15 15:08:44 +0100

    adding timer

---
+++ src/components/timer/timer.js
@@ -0,0 +1,49 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+
+import "./styles.css";
+
+const renderTime = ({ remainingTime }) => {
+  if (remainingTime === 0) {
+    return <div className="timer">Too lale...</div>;
+  }
+
+  return (
+    <div className="timer">
+      <div className="text">Remaining</div>
+      <div className="value">{remainingTime}</div>
+      <div className="text">seconds</div>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        CountdownCircleTimer
+        <br />
+        React Component
+      </h1>
+      <div className="timer-wrapper">
+        <CountdownCircleTimer
+          isPlaying
+          duration={10}
+          colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+          colorsTime={[10, 6, 3, 0]}
+          onComplete={() => ({ shouldRepeat: true, delay: 1 })}
+        >
+          {renderTime}
+        </CountdownCircleTimer>
+      </div>
+      <p className="info">
+        Change component properties in the code filed on the right to try
+        difference functionalities
+      </p>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
