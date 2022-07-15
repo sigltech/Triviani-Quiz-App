5ce65cc3968bd52eb3700177e238640b3b4de0a5
sigltech <101203107+sigltech@users.noreply.github.com>
2022-07-15 17:21:19 +0100

    online game page

--- src/pages/OnlineGamePage/index.jsx
+++ src/pages/OnlineGamePage/index.jsx
@@ -4,36 +4,49 @@ import './style.css'
 export default function OnlineGamePage() {
     return (
         <>
-        <div className="Online-game-start-container">
-            <h1>Play an Game</h1>
-            <form className="flex-col-center">
-                <label htmlFor="">Number Of Players</label>
-                <select aria-label="players-selector">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                </select>
-                <label htmlFor="">Number Of Questions</label>
-                <div className="questions-selection">
-                    <div>
-                        <label htmlFor="10-questions">10</label>
-                        <input id="10-questions" type="radio" name="questions-number" value="10" checked />
-                    </div>
+            <h1 className="online-game-heading">Online Game</h1>
 
-                    <div>
-                        <label htmlFor="15-questions">15</label>
-                        <input id="15-questions" type="radio" name="questions-number" value="15" />
+                <div className="form-container">
+                    <div className="create-game-container">
+                    <div className="create-game-button">
+                        <button>Create Game</button>
                     </div>
-
-                    <div>
-                        <label htmlFor="20-questions">20</label>
-                        <input id="20-questions" type="radio" name="questions-number" value="20" />
-                    </div>
+                    <form className="join-game-container">
+                        <label htmlFor="room-number">Type in Room number</label>
+                        <input type="number" name="room-number" id="room-number" />
+                        <button type="submit">Join game</button>
+                    </form>
                 </div>
+            </div>
+            {/* <div className="Online-game-start-container">
+                <form className="flex-col-center">
+                    <label htmlFor="">Number Of Players</label>
+                    <select aria-label="players-selector">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                    </select>
+                    <label htmlFor="">Number Of Questions</label>
+                    <div className="questions-selection">
+                        <div>
+                            <label htmlFor="10-questions">10</label>
+                            <input id="10-questions" type="radio" name="questions-number" value="10" checked />
+                        </div>
 
-            </form>
-        </div>
+                        <div>
+                            <label htmlFor="15-questions">15</label>
+                            <input id="15-questions" type="radio" name="questions-number" value="15" />
+                        </div>
+
+                        <div>
+                            <label htmlFor="20-questions">20</label>
+                            <input id="20-questions" type="radio" name="questions-number" value="20" />
+                        </div>
+                    </div>
+
+                </form>
+            </div> */}
         </>
     )
 }
