3aea6586a602cd75acf4f079d9d0e2ce30bda320
sigltech <101203107+sigltech@users.noreply.github.com>
2022-07-15 18:05:15 +0100

    Layouts folder created for motion background

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
-import { OnlineGamePage, GameFinishPage, GamePage } from './pages';
+import { OnlineGamePage, GameFinishPage, GamePage, IndexPage } from './pages';
+import Layout from './Layouts';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/">
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<IndexPage />} />
           <Route path="/online" element={<OnlineGamePage />} />
           <Route path="/finish" element={<GameFinishPage />} />
           <Route path="/game" element={<GamePage />} />
