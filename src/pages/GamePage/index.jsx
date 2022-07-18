bcb7f7cb7cf65479bf5cc6a6177ee3f7577a54af
Aaron Smith <2021manifest@gmail.com>
2022-07-18 16:32:57 +0100

    got score to increment on correct answer

--- src/pages/GamePage/index.jsx
+++ src/pages/GamePage/index.jsx
@@ -7,8 +7,8 @@ import { LoadingPage } from '../../components/index.jsx';
 import { handleScoreChange } from '../../redux/action';
 
 function GamePage() {
-    const [questionIndex, setQuestionIndex] = useState(0);
-    const [timer, setTimer] = useState(10000);
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [timer, setTimer] = useState(10000);
 
   const {
     question_category,
@@ -16,10 +16,10 @@ function GamePage() {
     question_type,
     questionsAmount,
     players,
-    intScore
+    intScore,
   } = useSelector((state) => state);
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   let apiUrl = `api.php?amount=${questionsAmount}`;
 
@@ -75,65 +75,78 @@ function GamePage() {
     }
   };
 
-    const handleAnswerSelect = (e) => {
-        console.log(e.target.textContent);
-        console.log(response.results[0].correct_answer)
-        console.log(typeof response.results[0].correct_answer)
-        console.log(typeof e.target.textContent)
-        if(e.target.textContent === response.results[questionIndex].correct_answer){
-            console.log(`Correct answer is ${response.results[questionIndex].correct_answer}`);
-            dispatch(handleScoreChange(intScore + 1))
-            console.log(intScore)
-            setQuestionIndex(questionIndex + 1);
-            setTimer(10000);
-           
-
-        } else {
-            console.log(`That's is the wrong answer`);
-        }
+  const handleAnswerSelect = (e) => {
+    // console.log(e.target.textContent);
+    // console.log(response.results[0].correct_answer);
+    // console.log(typeof response.results[0].correct_answer);
+    // console.log(typeof e.target.textContent);
+    if (
+      e.target.textContent === response.results[questionIndex].correct_answer
+    ) {
+      console.log(
+        `Correct answer is ${response.results[questionIndex].correct_answer}`
+      );
+      dispatch(handleScoreChange(intScore + 1));
+      console.log(intScore);
+      setQuestionIndex(questionIndex + 1);
+      setTimer(10000);
+    } else {
+      console.log(`That's is the wrong answer`);
     }
+  };
 
-
-        const RenderQuestions = () => {
+  const RenderQuestions = () => {
+    return (
+      <>
+        {response.results[questionIndex].incorrect_answers.map(
+          (answer, index) => {
             return (
-                <>
-                    {response.results[questionIndex].incorrect_answers.map((answer, index) => {
-                        return <div type="button" onClick={handleAnswerSelect} className="answerCard" key={index} dangerouslySetInnerHTML={{__html: answer}}></div>;
-                    })}
+              <div
+                type="button"
+                onClick={handleAnswerSelect}
+                className="answerCard"
+                key={index}
+                dangerouslySetInnerHTML={{ __html: answer }}
+              ></div>
+            );
+          }
+        )}
 
-                    <div onClick={handleAnswerSelect} className="answerCard">{response.results[questionIndex].correct_answer}</div>
-                </>
-            )
-        }
+        <div onClick={handleAnswerSelect} className="answerCard">
+          {response.results[questionIndex].correct_answer}
+        </div>
+      </>
+    );
+  };
 
   return (
     <div className="gamePage">
-      <div className='game-container'>
-                <div className='gamepage-container'>
+      <div className="game-container">
+        <div className="gamepage-container">
+          <div className="countDown">
+            <Countdown date={Date.now() + timer} renderer={renderer} />
+          </div>
 
-                    <div className="countDown">
-                    <Countdown date={Date.now() + timer} renderer={renderer} />
-                    </div>
+          <div>
+            <h1>{response.results[questionIndex].question}</h1>
+          </div>
 
-                    <div>
-                        <h1>{response.results[questionIndex].question}</h1>
-                    </div>
+          <button /*onClick={getApi}*/> press for questions </button>
 
-                    <button /*onClick={getApi}*/> press for questions </button>
-                    
-                    <div className="answers">
-                        
-                        <RenderQuestions />
-                            
-                        {/* <div className="answerCard"> <h4> answer1</h4> </div>
+          <div className="answers">
+            <RenderQuestions />
+
+            {/* <div className="answerCard"> <h4> answer1</h4> </div>
                         <div className="answerCard"> <h4> answer2</h4> </div>
                         <div className="answerCard"> <h4> answer3</h4> </div>
                         <div className="answerCard"> <h4> answer4</h4> </div> */}
-                    </div>
+          </div>
 
           <div>
-
-
+            <h3>
+              {' '}
+              Score: {intScore} / {response.results.length}
+            </h3>
             <button
               style={{ color: '#000' }}
               onClick={() => {
@@ -145,6 +158,7 @@ function GamePage() {
           </div>
         </div>
       </div>
+    </div>
   );
 }
 
