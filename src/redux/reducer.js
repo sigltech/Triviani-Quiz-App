f11c1b87d56287220490014f933e762644e7df67
sigltech <101203107+sigltech@users.noreply.github.com>
2022-07-20 17:38:38 +0100

    netlify build

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -17,7 +17,7 @@ const initState = {
   questionsAmount: 10,
   players: 1,
   intScore: 0,
-  player: [],
+  player: [{name: "", score: 0}],
 };
 const Reducer = (state = initState, action) => {
   console.log(state.player)
