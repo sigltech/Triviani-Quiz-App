94f6ecb9a1124d7140428645aa18b8c24d3b66ae
Aaron Smith <2021manifest@gmail.com>
2022-07-21 12:43:31 +0100

    google translate working

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,8 +1,9 @@
 export { default as LoadingPage } from './LoadingPage';
 export { default as SelectInputs } from './SelectInputs';
 export { default as Timer } from './Timer';
-export {default as DarkModeToggle} from './DarkModeToggle';
-export {default as LeaderBoardChart} from './LeaderBoardChart';
-export {default as LeaderBoardList} from './LeaderBoardList';
-export {default as RenderQuestions} from './RenderQuestions';
+export { default as DarkModeToggle } from './DarkModeToggle';
+export { default as LeaderBoardChart } from './LeaderBoardChart';
+export { default as LeaderBoardList } from './LeaderBoardList';
+export { default as RenderQuestions } from './RenderQuestions';
 export { default as AddUsername } from './AddUsername';
+export { default as GoogleTranslate } from './GoogleTranslate';
