67dc6c8b9b1064037fb3a3082dba9a35d9fc14a4
Aaron Smith <2021manifest@gmail.com>
2022-07-21 12:23:48 +0100

    questions are now random and correct answer value set

--- src/components/RenderQuestions/index.jsx
+++ src/components/RenderQuestions/index.jsx
@@ -1,27 +1,54 @@
+import { useEffect, useState } from 'react';
+
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+};
 
 const RenderQuestions = ({ response, questionIndex, handleAnswerSelect }) => {
+  const [options, setOptions] = useState([]);
+  const corr_answer = response.results[questionIndex].correct_answer;
 
-    return (
-      <>
-        {response.results[questionIndex].incorrect_answers.map(
-          (answer, index) => {
-            return (
-              <div
-                type="button"
-                onClick={handleAnswerSelect}
-                className="answerCard"
-                key={index}
-                dangerouslySetInnerHTML={{ __html: answer }}
-              ></div>
-            );
+  useEffect(() => {
+    if (response?.results.length) {
+      const question = response.results[questionIndex];
+      let answers = [...question.incorrect_answers];
+      let correct = question.correct_answer;
+
+      answers.splice(
+        getRandomInt(question.incorrect_answers.length),
+        0,
+        correct
+      );
+      console.log(`correct answers`, correct);
+      setOptions(answers);
+    }
+  }, [response, questionIndex]);
+  console.log(corr_answer);
+  return (
+    <>
+      {options &&
+        options.map((answer, index, value) => {
+          if (answer === corr_answer) {
+            value = 'correct';
           }
-        )}
+          console.log(`in return block`, answer, index);
+          return (
+            <div
+              type="button"
+              onClick={handleAnswerSelect}
+              className="answerCard"
+              value={value}
+              key={index}
+              dangerouslySetInnerHTML={{ __html: answer }}
+            ></div>
+          );
+        })}
 
-        <div onClick={handleAnswerSelect} className="answerCard">
-          {response.results[questionIndex].correct_answer}
-        </div>
-      </>
-    );
-  };
+      {/* <div onClick={handleAnswerSelect} className="answerCard">
+        {response.results[questionIndex].correct_answer}
+      </div> */}
+    </>
+  );
+};
 
-  export default RenderQuestions;
+export default RenderQuestions;
