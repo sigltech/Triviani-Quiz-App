9edee41e8993611f7b3d072a2615ea4eb0ca167f
Aaron Smith <2021manifest@gmail.com>
2022-07-21 15:30:56 +0100

    on devbranch by accident added empty local game/ selectinput test

--- src/components/SelectInputs/index.jsx
+++ src/components/SelectInputs/index.jsx
@@ -51,7 +51,8 @@ const SelectInputs = ({ label, apiData }) => {
         <option disabled className="" value="">
           {label}
         </option>
-        {apiData && apiData.map(({ id, name }) => (
+        {apiData &&
+          apiData.map(({ id, name }) => (
             <option value={id} key={id}>
               {name}
             </option>
